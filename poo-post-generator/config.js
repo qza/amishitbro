022b442eb292eac6d848b387b26bddbdf54880d3
zee <kokovic.zoran@gmail.com>
2026-04-16 06:47:42 +0200

    feat: add poo-post-generator config

---
+++ poo-post-generator/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  model: 'claude-haiku-4-5-20251001',
+  systemPrompt:
+    'You are a poo science expert. You know surprising, funny, and educational facts about human digestion and excrement. Be witty but factual.',
+  userPrompt:
+    'Give me one surprising poo science fact. Keep it to 1-2 sentences. Make it funny but true.',
+  siteUrl: 'amishitbro.com',
+}
